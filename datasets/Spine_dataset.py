4836cbec74fee0f9a952fd74b80f40549307fe65
arnav0400 <Arnav0400>
2019-12-31 15:59:40 +0530

    Updated Custom DataLoader

--- datasets/Spine_dataset.py
+++ datasets/Spine_dataset.py
@@ -7,21 +7,12 @@ import torchvision.transforms as transforms
 import cv2
 import numpy as np
 
-IMAGES = 'images'
-ANNOTATIONS = 'annotations'
-INSTANCES_SET = 'instances_{}.json'
-_CLASSES = ('vertebrae')
 
 class SPINEDetection(data.Dataset):
 
     def __init__(self, root, bboxes_df, corners_df, fileame_df, image_set='training',
      transform=None, dataset_name=''):
-        # sys.path.append(osp.join(root, COCO_API))
-        # from pycocotools.coco import COCO
-        self.root = osp.join(root, IMAGES, image_set)
-        # self.coco = COCO(osp.join(root, ANNOTATIONS,
-        #                           INSTANCES_SET.format(image_set)))
-        # self.ids = list(self.coco.imgToAnns.keys())
+        self.root = osp.join(root, image_set)
         self.transform = transform
         self.name = dataset_name
         self.bboxes_df = bboxes_df
@@ -31,24 +22,20 @@ class SPINEDetection(data.Dataset):
     def __getitem__(self, index):
         img_id = self.fileame_df.iloc[index,0]
         bboxes = self.bboxes_df[self.bboxes_df.image_id == img_id]
-        corners = self.corners_df[self.corners_df.image_id == img_id]
+        corners = self.corners_df.iloc[index,:]
+        corner_arr = []
+        for i in range(17):
+            x1,x2,x3,x4 = corners.iloc[4*i:4*(i+1)]
+            y1,y2,y3,y4 = corners.iloc[68+4*i:68+4*(i+1)]
+            corner_arr.append(np.array([x1,x2,x3,x4,y1,y2,y3,y4]))
+
+        corner_arr = np.array(corner_arr)
 
         img = cv2.imread(osp.join(self.root, img_id))
 
-        ##########
-
-        # Needs Transformation to model
-        
-        ##########
-
-        # if self.transform is not None:
-        #     annotation = {'image': img, 'bboxes': bbox, 'category_id': labels}
-        #     augmentation = self.transform(**annotation)
-        #     img = augmentation['image']
-        #     bbox = augmentation['bboxes']
-        #     labels = augmentation['category_id']
-
-        return {'image': img, 'bboxes': bbox, 'corners': corners, 'category_id': labels} 
+        bbox = bboxes.iloc[:,1:5].values
+        labels = bboxes.iloc[:,5].values
+        return {'image': img, 'bboxes': bbox, 'corners': corner_arr, 'category_id': labels} 
 
 
     def __len__(self):
