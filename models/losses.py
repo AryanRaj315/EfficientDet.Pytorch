8b9434a0dc795ca30ae5b9caefa06749b6a450ec
arnav0400 <Arnav0400>
2020-01-01 18:24:56 +0530

    deleted L2 loss

--- models/losses.py
+++ models/losses.py
@@ -50,10 +50,11 @@ class FocalLoss(nn.Module):
 
             corner_annotation = annotations_corners[j, :, :]
 
+            # compute the loss for corner regression
+
             if bbox_annotation.shape[0] == 0:
                 regression_losses.append(torch.tensor(0).float().to(anchors.device))
                 classification_losses.append(torch.tensor(0).float().to(anchors.device))
-                corner_losses.append(torch.tensor(0).float().to(anchors.device))
                 continue
 
             classification = torch.clamp(classification, 1e-4, 1.0 - 1e-4)
@@ -65,7 +66,7 @@ class FocalLoss(nn.Module):
             #import pdb
             #pdb.set_trace()
 
-            # compute the loss for regression
+            # compute the loss for classification
             targets = torch.ones(classification.shape) * -1
             targets = targets.to(anchors.device)
 
@@ -95,10 +96,51 @@ class FocalLoss(nn.Module):
 
             classification_losses.append(cls_loss.sum()/torch.clamp(num_positive_anchors.float(), min=1.0))
 
-            # compute the loss for corners
-            ###########
-            # ADD!!!
-            ###########
+            # compute the loss for regression
 
+            if positive_indices.sum() > 0:
+                assigned_annotations = assigned_annotations[positive_indices, :]
+
+                anchor_widths_pi = anchor_widths[positive_indices]
+                anchor_heights_pi = anchor_heights[positive_indices]
+                anchor_ctr_x_pi = anchor_ctr_x[positive_indices]
+                anchor_ctr_y_pi = anchor_ctr_y[positive_indices]
+
+                gt_widths  = assigned_annotations[:, 2] - assigned_annotations[:, 0]
+                gt_heights = assigned_annotations[:, 3] - assigned_annotations[:, 1]
+                gt_ctr_x   = assigned_annotations[:, 0] + 0.5 * gt_widths
+                gt_ctr_y   = assigned_annotations[:, 1] + 0.5 * gt_heights
+
+                # clip widths to 1
+                gt_widths  = torch.clamp(gt_widths, min=1)
+                gt_heights = torch.clamp(gt_heights, min=1)
+
+                targets_dx = (gt_ctr_x - anchor_ctr_x_pi) / anchor_widths_pi
+                targets_dy = (gt_ctr_y - anchor_ctr_y_pi) / anchor_heights_pi
+                targets_dw = torch.log(gt_widths / anchor_widths_pi)
+                targets_dh = torch.log(gt_heights / anchor_heights_pi)
+
+                targets = torch.stack((targets_dx, targets_dy, targets_dw, targets_dh))
+                targets = targets.t()
+
+                targets = targets/torch.Tensor([[0.1, 0.1, 0.2, 0.2]]).to(anchors.device)
+
+
+                negative_indices = ~positive_indices
+
+                regression_diff = torch.abs(targets - regression[positive_indices, :])
+
+                regression_loss = torch.where(
+                    torch.le(regression_diff, 1.0 / 9.0),
+                    0.5 * 9.0 * torch.pow(regression_diff, 2),
+                    regression_diff - 0.5 / 9.0
+                )
+                regression_losses.append(regression_loss.mean())
+            else:
+                regression_losses.append(torch.tensor(0).float().to(anchors.device))
+
+
+            ##############################
+            # compute the loss for corners
 
         return torch.stack(classification_losses).mean(dim=0, keepdim=True), torch.stack(regression_losses).mean(dim=0, keepdim=True), torch.stack(corner_losses).mean(dim=0, keepdim=True)
